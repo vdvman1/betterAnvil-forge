78776f0dc7ed25f087a4e7eaa7bcfe70c940beb7
Keenan Scott <eggdropsoap@pipemaze.com>
2013-08-11 03:56:31 -0700

    Removed unnecessary overrides

--- vdvman1/betterAnvil/block/BlockAnvilBA.java
+++ vdvman1/betterAnvil/block/BlockAnvilBA.java
@@ -11,50 +11,10 @@ import cpw.mods.fml.relauncher.SideOnly;
 
 public class BlockAnvilBA extends BlockAnvil
 {
-    private static final String[] textureNames = new String[] {"anvil_top", "anvil_top_damaged_1", "anvil_top_damaged_2"};
-    @SideOnly(Side.CLIENT)
-    private Icon[] iconArray;
 
     public BlockAnvilBA(int id)
     {
         super(id);
-    }
-
-    @SideOnly(Side.CLIENT)
-
-    /**
-     * From the specified side and block metadata retrieves the blocks texture. Args: side, metadata
-     */
-    @Override
-    public Icon getIcon(int side, int metadata)
-    {
-        if (this.field_82521_b == 3 && side == 1) //this.field_82521_b is from normal anvil, is used to represent the section of the anvil being represented, from bottom to top, 0-3
-        {
-            int k = (metadata >> 2) % this.iconArray.length;
-            return this.iconArray[k];
-        }
-        else
-        {
-            return this.blockIcon;
-        }
-    }
-
-    @SideOnly(Side.CLIENT)
-
-    /**
-     * When this method is called, your block should register all the icons it needs with the given IconRegister. This
-     * is the only chance you get to register icons.
-     */
-    @Override
-    public void registerIcons(IconRegister iconRegister)
-    {
-        this.blockIcon = iconRegister.registerIcon("anvil_base");
-        this.iconArray = new Icon[textureNames.length];
-
-        for (int i = 0; i < this.iconArray.length; ++i)
-        {
-            this.iconArray[i] = iconRegister.registerIcon(textureNames[i]);
-        }
     }
 
     /**
