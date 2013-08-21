64b9e0d87da6194e7c459d214515eac469354ab6
vdvman1 <vdvman88@gmail.com>
2013-08-21 17:23:04 +1000

    moved source to common folder

--- common/vdvman1/betterAnvil/block/BlockAnvilBA.java
+++ common/vdvman1/betterAnvil/block/BlockAnvilBA.java
@@ -1,13 +1,9 @@
 package vdvman1.betterAnvil.block;
 
 import net.minecraft.block.BlockAnvil;
-import net.minecraft.client.renderer.texture.IconRegister;
 import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.util.Icon;
 import net.minecraft.world.World;
 import vdvman1.betterAnvil.BetterAnvil;
-import cpw.mods.fml.relauncher.Side;
-import cpw.mods.fml.relauncher.SideOnly;
 
 public class BlockAnvilBA extends BlockAnvil
 {
@@ -23,14 +19,7 @@ public class BlockAnvilBA extends BlockAnvil
     @Override
     public boolean onBlockActivated(World world, int x, int y, int z, EntityPlayer entityPlayer, int sideHit, float sideHitX, float sideHitY, float sideHitZ)
     {
-        if (world.isRemote)
-        {
-            return true;
-        }
-        else
-        {
-            entityPlayer.openGui(BetterAnvil.instance, 0, world, x, y, z);
-            return true;
-        }
+        entityPlayer.openGui(BetterAnvil.instance, 0, world, x, y, z);
+        return true;
     }
 }
