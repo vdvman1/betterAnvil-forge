53f3c052bdc97b0b4c322dfd96f295e3eefce68b
master801 <master801@users.noreply.github.com>
2015-06-14 15:51:04 -0500

    3.2.0 update
    Adding in a new recipe for the better anvil without stupidly broken
    substitution.

--- src/main/java/vdvman1/betterAnvil/BetterAnvil.java
+++ src/main/java/vdvman1/betterAnvil/BetterAnvil.java
@@ -1,30 +1,38 @@
 package vdvman1.betterAnvil;
 
-import cpw.mods.fml.common.*;
+import cpw.mods.fml.common.FMLCommonHandler;
+import cpw.mods.fml.common.Loader;
+import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.EventHandler;
 import cpw.mods.fml.common.Mod.Instance;
-import cpw.mods.fml.common.event.*;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLMissingMappingsEvent;
 import cpw.mods.fml.common.event.FMLMissingMappingsEvent.MissingMapping;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 import cpw.mods.fml.common.network.NetworkRegistry;
-import cpw.mods.fml.common.registry.ExistingSubstitutionException;
 import cpw.mods.fml.common.registry.GameRegistry;
-import cpw.mods.fml.common.registry.GameRegistry.Type;
 import net.minecraft.enchantment.Enchantment;
 import net.minecraft.init.Blocks;
+import net.minecraft.init.Items;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemAnvilBlock;
+import net.minecraft.item.ItemStack;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.common.config.Configuration;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import vdvman1.betterAnvil.block.BlockAnvilBA;
-import vdvman1.betterAnvil.common.*;
+import vdvman1.betterAnvil.common.Config;
+import vdvman1.betterAnvil.common.EventHandlerBA;
+import vdvman1.betterAnvil.common.GuiHandler;
+import vdvman1.betterAnvil.common.Utils;
 
 import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
-@Mod(modid = BetterAnvil.MOD_ID, name = BetterAnvil.MOD_NAME, version = BetterAnvil.VERSION, dependencies = "required-after:Forge@[10.13.2.1342,11]", guiFactory = "vdvman1.betterAnvil.client.gui.config.ConfigFactoryBA")
+@Mod(modid = BetterAnvil.MOD_ID, name = BetterAnvil.MOD_NAME, version = BetterAnvil.VERSION, guiFactory = "vdvman1.betterAnvil.client.gui.config.ConfigFactoryBA")
 public final class BetterAnvil {
 
     //Global variables
@@ -33,10 +41,7 @@ public final class BetterAnvil {
     public static final Logger BETTER_ANVIL_LOGGER = LogManager.getLogger(BetterAnvil.MOD_NAME);
 
     //Blocks
-    public static final BlockAnvilBA BLOCK_ANVIL = new BlockAnvilBA();
-
-    //Items
-    public static final ItemAnvilBlock ITEM_BLOCK_ANVIL = new ItemAnvilBlock(BetterAnvil.BLOCK_ANVIL);
+    public static final BlockAnvilBA BLOCK_BETTER_ANVIL = new BlockAnvilBA();
 
     @Instance(BetterAnvil.MOD_ID)
     public static BetterAnvil instance;
@@ -46,18 +51,14 @@ public final class BetterAnvil {
     public void preInit(FMLPreInitializationEvent event) {
         Config.setConfiguration(new Configuration(new File(Loader.instance().getConfigDir(), "BetterAnvil.cfg")));
         Config.syncConfiguration(true);
+
+        GameRegistry.registerBlock(BetterAnvil.BLOCK_BETTER_ANVIL, ItemAnvilBlock.class, "better_anvil");
     }
 
     //Called during initialization, used for registering everything etc.
     @EventHandler
     public void init(FMLInitializationEvent event) {
-        //Why do we have to add substitution aliases here? It makes absolutely no sense. >_<
-        try {
-            GameRegistry.addSubstitutionAlias("minecraft:anvil", Type.BLOCK, BetterAnvil.BLOCK_ANVIL);
-            GameRegistry.addSubstitutionAlias("minecraft:anvil", Type.ITEM, BetterAnvil.ITEM_BLOCK_ANVIL);
-        } catch(ExistingSubstitutionException e) {
-            e.printStackTrace();
-        }
+        GameRegistry.addShapedRecipe(new ItemStack(BetterAnvil.BLOCK_BETTER_ANVIL, 1, 0), "XYX", "XXX", 'X', Items.iron_ingot, 'Y', Blocks.anvil);
 
         MinecraftForge.EVENT_BUS.register(EventHandlerBA.INSTANCE);
         FMLCommonHandler.instance().bus().register(EventHandlerBA.INSTANCE);
@@ -94,10 +95,10 @@ public final class BetterAnvil {
             if (missingMapping.name.equals("BetterAnvil:anvilba") | missingMapping.name.equals("minecraft:anvil")) {
                 switch(missingMapping.type) {
                     case BLOCK:
-                        missingMapping.remap(Blocks.anvil);//Remap to Minecraft's anvil block [which is substituted].
+                        missingMapping.remap(BetterAnvil.BLOCK_BETTER_ANVIL);
                         break;
                     case ITEM:
-                        missingMapping.remap(Item.getItemFromBlock(Blocks.anvil));//Remap to Minecraft's anvil item block [which is substituted].
+                        missingMapping.remap(Item.getItemFromBlock(BetterAnvil.BLOCK_BETTER_ANVIL));
                         break;
                 }
             }
